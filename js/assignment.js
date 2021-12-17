4f5a41eeb4a2a465341e278c7dceb72beaf186c2
mushtaq1767 <mohammedmushtaq313@gmail.com>
2021-12-17 19:20:20 +0530

    message

---
+++ js/assignment.js
@@ -0,0 +1,85 @@
+//function myFun(name){
+//name=name.toLowerCase();
+//let charObj={}
+//for(let i=0;i<name.length;i++){
+	//if(name[i] !== ""){
+	//if(charObj[name[i]]==undefined){
+		//charObj[name[i]]=1
+	//}else{
+//		charObj[name[i]]=charObj[i]]+1
+ //}
+// }
+//}
+//return charObj
+//}
+//console.log(myFun('sudaksha pvt'))
+
+
+
+
+
+//function multiply(a,b){
+//return a*b 
+//}
+//test=multiply
+//function square(a,b,test){
+	//c=(a*a)+(b*b)+2*(test(a,b))
+	//console.log(c)
+//}
+
+//sqaure(2,3,multiply)//
+
+function details1(address,doorno,street,city){
+address= madhapur
+console.log(address)
+details2(doorno)
+}
+
+function details2(doorno){
+doorno= 11-23/12
+console.log(doorno)
+details3(street)
+}
+function details3(street){
+street= ayyapa society
+console.log(street)
+details4(city)
+}
+
+function details4(city){
+city= hyderabad
+console.log(city)
+}
+details1(address,doorno,street,city)
+
+
+
+function abc(a,b,product,sub)
+	{ 
+	var c= a+b;
+	console.log(c);
+	cab(a,b)
+	}
+	
+	function cab(a,b,sub){
+		var k=a*b;
+		console.log(k);
+		bac(a,b);
+	}
+	function bac(a,b)
+	{
+		var x=a-b;
+		console.log(x);
+	}
+	abc(2,4,cab,bac);
+
+
+
+
+
+
+
+
+
+
+
